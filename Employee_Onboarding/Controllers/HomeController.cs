5ea185191e55a9081b6a3bb47c4771d50c524526
codeamrit <amritag2004@gmail.com>
2022-07-17 20:56:54 +0530

    initial

---
+++ Employee_Onboarding/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Diagnostics;
+using WebApplication2.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SaveEmployee(EmployeeModel emp)
+        {
+            var client = new RestClient("http://localhost:5019/api");
+            //.Timeout = -1;
+            var request = new RestRequest("/employee/SaveEmployee", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(emp);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return View("Employee");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteEmployee(string EmpId)
+        {
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = EmpId;
+            var client = new RestClient("http://localhost:5019/api");
+            //.Timeout = -1;
+            var request = new RestRequest("/employee/DeleteEmployee", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(emp);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return RedirectToAction("EmployeeList", "Home");
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateEmployee(EmployeeModel emp)
+        {
+            var client = new RestClient("http://localhost:5019/api");
+            //.Timeout = -1;
+            var request = new RestRequest("/employee/UpdateEmployee", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(emp);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return View("Employee");
+        }
+        public IActionResult GetEmployeeDetail(string EmpId)
+        {
+            var client = new RestClient("http://localhost:5019/api");
+            //.Timeout = -1;
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = EmpId;
+
+            var request = new RestRequest("/employee/GetEmployee", Method.Get);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(emp);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            List<EmployeeModel> lstemp = JsonConvert.DeserializeObject<List<EmployeeModel>>(response.Content);
+            //Console.WriteLine(response.Content);
+            return View("UpdateEmployee",lstemp.FirstOrDefault());
+        }
+
+        public IActionResult EmployeeList()
+        {
+            var client = new RestClient("http://localhost:5019/api");
+            //.Timeout = -1;
+            EmployeeModel emp = new EmployeeModel();
+
+            var request = new RestRequest("/employee/GetEmployee", Method.Get);
+            request.AddHeader("Content-Type", "application/json");
+            var body = JsonConvert.SerializeObject(emp);
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            List<EmployeeModel> lstemp = JsonConvert.DeserializeObject<List<EmployeeModel>>(response.Content);
+            //Console.WriteLine(response.Content);
+            return View(lstemp);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Employee_Onboarding/Models/EmployeeModel.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class EmployeeModel
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+
+        [BsonIgnore]
+        public string? EmpId { get; set; }
+
+        //[Required]
+        public string? Adress { get; set; }
+
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int Pincode { get; set; }
+        public string Title { get; set; }
+        public int Dob { get; set; }
+
+        //[Required]
+        public string? Namefirst { get; set; }
+
+        //[Required]
+        public string? Namelast { get; set; }
+
+        public string? ProfilePicture { get; set; }
+        public string? EducationalDocument { get; set; }
+        public string? Department { get; set; }
+        //[Required]
+        public int ContactNumber { get; set; }
+
+        [EmailAddress]
+        public string? EmailId { get; set; }
+    }
+}
